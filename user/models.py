bcfe152a230357ef0b86a5164ea1b22d8c4c28cc
rookieno <rhkdals3257@naver.com>
2022-01-25 15:58:14 +0900

    follow

--- user/models.py
+++ user/models.py
@@ -1,6 +1,7 @@
 #user/models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 
 # Create your models here.
@@ -8,3 +9,4 @@ class UserModel(AbstractUser):
     class Meta:
         db_table = "my_user"
     bio = models.CharField(max_length=256, default='')
+    follow = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followee')
